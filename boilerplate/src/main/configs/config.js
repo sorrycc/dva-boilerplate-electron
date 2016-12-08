4b118c6568909a1b687e9c19634e31b6f90b7be2
sorrycc <sorrycc@gmail.com>
2016-12-08 22:06:38 +0800

    add electron-config and electron-log

---
+++ boilerplate/src/main/configs/config.js
@@ -0,0 +1,3 @@
+import Config from 'electron-config';
+
+export default new Config({ name: 'config' });
--- boilerplate/src/main/index.js
+++ boilerplate/src/main/index.js
@@ -2,13 +2,20 @@
 import { app, BrowserWindow } from 'electron';
 import is from 'electron-is';
 import { join } from 'path';
+import log from 'electron-log';
 import * as application from './services/application';
+import * as window from './services/window';
+import * as config from './configs/config';
+
+log.info('(main/index) >>>>>>>>>>>>>>>>>>');
+log.info('(main/index) app start');
 
 if (is.dev()) {
   require('electron-debug')();
 }
 
 app.on('ready', () => {
+  log.info('(main/index) app ready');
   application.init();
 
   // 加载 devtools extension
@@ -21,3 +28,17 @@ app.on('ready', () => {
     );
   }
 });
+
+app.on('quit', () => {
+  log.info('(main/index) app quit');
+  log.info('(main/index) <<<<<<<<<<<<<<<<<<<');
+});
+
+// Register to global, so renderer can access these with remote.getGlobal
+global.services = {
+  application,
+  window,
+};
+global.configs = {
+  config,
+};

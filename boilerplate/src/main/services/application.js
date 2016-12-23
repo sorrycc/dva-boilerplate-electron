5ddaad8e5577d6e046dba1c0b506f6baa52ee70c
smackxu <smackxu@raventech.com>
2016-12-23 12:15:03 +0800

    Move dev handle to window.js form application.js

--- boilerplate/src/main/services/application.js
+++ boilerplate/src/main/services/application.js
@@ -1,14 +1,6 @@
-import is from 'electron-is';
-import { join } from 'path';
-import { create } from './window';
+import { create, getPath } from './window';
 
 export function init() {
   const win = create({ width: 800, height: 600 });
-  let path = `file://${join($dirname, '..', 'pages')}/main.html`;
-  if (is.dev()) {
-    const { serverIp, serverPort } = require('../../../webpack.config.dev.babel.js'); // eslint-disable-line global-require
-    path = `http://${serverIp}:${serverPort}/main-dev.html`;
-  }
-
-  win.loadURL(path);
+  win.loadURL(getPath());
 }
